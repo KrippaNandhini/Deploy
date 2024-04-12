6223303e5ad6364685417612e09e722b6e774b31
KrippaNandhini <79848429+KrippaNandhini@users.noreply.github.com>
2024-04-12 07:51:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pickle
 import streamlit as st
 
 
-loaded_model = pickle.load(open("D:/DS course/Deploy/trained_model.sav", 'rb'))
+loaded_model = pickle.load(open("trained_model.sav", 'rb'))
 
 
 # creating a function for Prediction
